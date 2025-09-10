658266e6f78810e8b68252a7ee40ae14d391cd6b
mkaraca <mkaraca@outlook.com>
2025-09-10 20:04:00 +0300

    fixed backtotop

--- components/elements/BackToTop.js
+++ components/elements/BackToTop.js
@@ -1,26 +1,27 @@
 import { useEffect, useState } from "react";
 
 function BackToTop() {
-    const [hasScrolled, setHasScrolled] = useState("false");
+    const [hasScrolled, setHasScrolled] = useState(false);
+
     useEffect(() => {
+        const onScroll = () => {
+            if (window.scrollY > 1000 && !hasScrolled) {
+                setHasScrolled(true);
+            } else if (window.scrollY <= 1000 && hasScrolled) {
+                setHasScrolled(false);
+            }
+        };
+
         window.addEventListener("scroll", onScroll);
         return () => {
             window.removeEventListener("scroll", onScroll);
         };
-    });
-
-    const onScroll = () => {
-        if (window.scrollY > 1000 && !hasScrolled) {
-            setHasScrolled(true);
-        } else if (window.scrollY < 1000 && hasScrolled) {
-            setHasScrolled(false);
-        }
-    };
+    }, [hasScrolled]);
 
     return (
         <>
             {hasScrolled && (
-                <a className={`btn-backToTop ${hasScrolled && "show"}`} href="#top"><i className="fa-solid fa-arrow-up"></i></a>
+                <a className="btn-backToTop show" href="#top"><i className="fa-solid fa-arrow-up"></i></a>
             )}
         </>
     );
