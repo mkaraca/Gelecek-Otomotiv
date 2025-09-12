ea80a4a998709adc09afedbe310fe81d05773ef0
mkaraca <mkaraca@outlook.com>
2025-09-12 19:26:09 +0300

    optimize and degrade

--- components/sections/Home/SliderMain.js
+++ components/sections/Home/SliderMain.js
@@ -35,8 +35,12 @@ function SliderMain() {
             <section className="slider-home-main">
                 <Swiper {...swiperOptions} className="swiper-wrapper">
                     <SwiperSlide>
-                        <video className="slider-home-main__video" ref={videoRef} muted loop playsInline autoPlay>
-                            <source src="assets/video/mb-slider-video-1.mp4" type="video/mp4" />
+                        <div className="slider-home-main__degrade"></div>
+                        <video className="slider-home-main__video slider-desktop" ref={videoRef} muted loop playsInline autoPlay>
+                            <source src="assets/video/mb-slider-video-1-desktop.mp4" type="video/mp4" />
+                        </video>
+                        <video className="slider-home-main__video slider-mobile" ref={videoRef} muted loop playsInline autoPlay>
+                            <source src="assets/video/mb-slider-video-1-mobile.mp4" type="video/mp4" />
                         </video>
                         <div className="container">
                             <div className="slider-home-main__content">
@@ -50,19 +54,22 @@ function SliderMain() {
                         </div>
                     </SwiperSlide>
                     <SwiperSlide>
-                        <img className="slider-home-main__image" src="assets/images/slider/slider-image-1.jpg" alt="Gelecek Otomotiv" />
+                        <div className="slider-home-main__degrade"></div>
+                        <img className="slider-home-main__image slider-mobile" src="assets/images/slider/slider-image-2-mobile.jpg" alt="Gelecek Otomotiv" />
+                        <img className="slider-home-main__image slider-desktop" src="assets/images/slider/slider-image-2-desktop.jpg" alt="Gelecek Otomotiv" />
                         <div className="container">
                             <div className="slider-home-main__content">
-                                <h2 className="title text-center text-black wow fadeInUp" data-depth="0.03" data-wow-delay="00ms" data-wow-duration="1500ms">
+                                <h2 className="title text-center text-white wow fadeInUp" data-depth="0.03" data-wow-delay="00ms" data-wow-duration="1500ms">
                                     Tamamen Elektrikli, Tamamen Mercedes.</h2>
                                 <div className="slider-button-wr">
-                                    <Link className="slider-button button-black wow fadeInUp" data-depth="0.05" href="/page-about"
+                                    <Link className="slider-button button-white wow fadeInUp" data-depth="0.05" href="/page-about"
                                         data-wow-delay="400ms" data-wow-duration="1500ms">TEST SÜRÜŞÜ İÇİN RANDEVU AL<i className="deg45 fa-solid fa-arrow-right"></i></Link>
                                 </div>
                             </div>
                         </div>
                     </SwiperSlide>
                     <SwiperSlide>
+                        {/* <div className="slider-home-main__degrade"></div> */}
                         <div className="container position-absolute start-0 end-0">
                             <div className="slider-home-main__content">
                                 <h2 className="title text-center text-black wow fadeInUp" data-depth="0.03" data-wow-delay="00ms" data-wow-duration="1500ms">
